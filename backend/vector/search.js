6028335a480099e60e1583135a9958784a6b6c71
ItsOnlyMe01 <134477804+ItsOnlyMe01@users.noreply.github.com>
2026-02-16 19:09:24 +0530

    fixed

--- backend/vector/search.js
+++ backend/vector/search.js
@@ -1,21 +1,15 @@
 const { client } = require("./chroma");
-const { embedText } = require("../utils/embedder");
 
 async function searchSimilar(query, workspaceId, topK = 5) {
   const collection = await client.getOrCreateCollection({
     name: "documents",
-    embeddingFunction: null,
   });
 
-  const queryEmbedding = await embedText(query);
-
   const results = await collection.query({
-    queryEmbeddings: [queryEmbedding],
+    queryTexts: [query],
     nResults: topK,
     where: { workspaceId: String(workspaceId) },
   });
-
-  console.log("RAW SEARCH RESULT:", results);
 
   return results.documents[0] || [];
 }
